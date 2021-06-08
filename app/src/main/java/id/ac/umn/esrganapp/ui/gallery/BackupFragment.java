2b23e217e4ce4cbabdacac7407aa78add995a065
unknown <gilbert.haensel@gmail.com>
2021-06-08 19:41:06 +0700

    2 image backup and nobackup

--- app/src/main/java/id/ac/umn/esrganapp/ui/gallery/BackupFragment.java
+++ app/src/main/java/id/ac/umn/esrganapp/ui/gallery/BackupFragment.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -76,6 +77,8 @@ public class BackupFragment extends Fragment implements GalleryRecyclerViewAdapt
                     startActivity(intent);
                 }
             });
+            ImageView img= root.findViewById(R.id.onboarding_logo_app);
+            img.setImageResource(R.drawable.backup);
         }
         else {
             List<String> mainStorageUri = ((MainActivity)getActivity()).getStorageUri();
